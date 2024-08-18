d6aeebf978b2a081ed0473ad2a9cda27c8a6fc2f
Karin <karin.metzgar@gmail.com>
2024-08-18 12:48:49 -0700

    IT WORKS!!!!!!

--- WK10 HW1/FT.cpp
+++ WK10 HW1/FT.cpp
@@ -11,38 +11,32 @@ vector<double> getAnalyticalSolution(double L, int mode, int N){
 
     vector<double> phi_x(N);
     double c = 2 * M_PI * mode / L;
+    double dx;
+    dx = L / N;
 
     for(int i = 0; i<N; i++){
-        phi_x[i] = (1/ (c * c)) * sin(c * i);
+
+        double x;
+        x = i * dx;
+        phi_x[i] = sin(c * x) / (c * c);
     }
     
     return phi_x;
 }
 
 // Discrete Fourier Transform (DFT)
-vector<complex<double>> DFT(vector<complex<double>>& F, double L, int N) {
+vector<complex<double>> DFT(vector<complex<double>>& F, int N) {
     vector<complex<double>> fs_phi_x(N);
     complex<double> i(0, 1);  // imaginary unit i = sqrt(-1)
     
     for(int k = 0; k<N; k++){
         complex<double> sum = 0.0;
-        fs_phi_x[0] = 0.0;
-        for(int n = 1; n<N; n++){
+        fs_phi_x[k] = 0.0;
+        for(int n = 0; n<N; n++){
             double c = (2 * M_PI * k * n) / N;
-            //sum += function[n] * exp(-i * angle);
             fs_phi_x[k] += F[n] * exp(-i * c);
         }
-        //fs_phi_x[k] += sum;
     }
-
-    //incorporate wave number, -k^2 term
-    /*
-    for (int k = 1; k<N; k++) {
-        double k_val = 2 * M_PI * k / L; //wave number
-        fs_phi_x[k] = fs_phi_x[k] / (-k_val * k_val);
-    }
-    fs_phi_x[0] = 0; */
-    
 
     return fs_phi_x;
 }
@@ -61,10 +55,19 @@ vector<complex<double>> DFT_derivative(vector<complex<double>>& F, double L, int
     for (int i = (N / 2) + 1; i < N; i++) {
         k[i] = (i - N) * dk;
     }
-
+    /*
     for (int i = 0; i < N; i++) {
         DFDT[i] = F[i] * complex<double>(0, 1) * k[i]; // Apply i*k
     }
+    */
+
+    for (int i = 0; i < N; i++) {
+    if (k[i] != 0) {
+        DFDT[i] = -F[i] / (k[i] * k[i]); 
+    } else {
+        DFDT[i] = 0; // Handle k[i] = 0 case
+    }
+}
 
     return DFDT;
 
@@ -72,8 +75,7 @@ vector<complex<double>> DFT_derivative(vector<complex<double>>& F, double L, int
 
 
 // Inverse Discrete Fourier Transform (IDFT)
-vector<complex<double>> I_DFT(const vector<complex<double>>& F) {
-    int N = F.size();
+vector<complex<double>> I_DFT(const vector<complex<double>>& F, int N) {
     vector<complex<double>> ft_phi_x(N);
     complex<double> i(0, 1);  // imaginary unit i = sqrt(-1)
 
@@ -95,39 +97,44 @@ int main() {
     double L = 2 * M_PI;
     int mode = 2;
     int N = 64;
-    const double eps0 = 8.854187817e-12;
+    const double eps0 = 8.854e-12;
+
+    //get the analytical solution
     vector<double> analysticalSolution = getAnalyticalSolution(L, mode, N);
 
 
-    double c = 2 * M_PI * mode / L;
     vector<complex<double>> rho(N); 
     vector<complex<double>> function(N);
     vector<complex<double>> fs_phi_x(N);
     vector<complex<double>> DTFT(N);
     vector<complex<double>> ft_phi_x(N);
     
-     
+    double dx;
+    dx = L / N;
+    double x;
 
+    double c = 2 * M_PI * mode / L;
     for (int i = 0; i < N; i++) {
-        rho[i] = eps0 * sin(c * i);
+        x = i * dx;
+        rho[i] = eps0 * sin(c * x);
         function[i] = -rho[i] / eps0;
     }
 
     //compute the Fourier Transform of the function
-    fs_phi_x = DFT(function, L, N);
+    fs_phi_x = DFT(function, N);
 
     //compute the derivative in the transformed domain
     DTFT = DFT_derivative(fs_phi_x, L, N);
 
     //compute the inverse Fourier Transform of the derivative
-    ft_phi_x = I_DFT(DTFT);
+    ft_phi_x = I_DFT(DTFT, N);
 
 
     ofstream out("FT.csv");
     out << "x, analytical, FT_real" << endl;
     // Write each vector element to the file in a new line
     for (size_t i = 0; i < N; i++) {
-        out << i << "," << analysticalSolution[i] << "," << real(ft_phi_x[i]) << endl;
+        out << i << "," << analysticalSolution[i] << ","  << real(ft_phi_x[i]) << endl;
      }
     out.close();
 
